14201c57fb2241fc5b840be70873b1ca560871c1
SamarMorsy <samar.magdy.ahmed@gmail.com>
2023-08-10 12:49:20 -0700

    "commit"

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+/**
+ * main - Entry point
+ *
+ * Descreption: A c program that print text using print f
+ *
+ * Return: alwayes 0 (Success)
+*/
+#include<stdio.h>
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}

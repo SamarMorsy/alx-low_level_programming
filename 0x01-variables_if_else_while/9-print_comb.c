4d20b713100d190cc1164be525a2515a1d950485
SamarMorsy <samar.magdy.ahmed@gmail.com>
2023-08-11 20:05:07 -0700

    "commit"

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+/**
+ * main - entry point
+ *
+ * Descreption: 0-9 usig -
+ *
+ * Return: alwayes 0 (success)
+*/
+#include <stdio.h>
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i + '0');
+		if (!(i == 9))
+			putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+}

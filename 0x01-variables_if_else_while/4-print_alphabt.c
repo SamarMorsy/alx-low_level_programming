f98148a94558e55246e7b73294bfbaed1cfd941e
SamarMorsy <samar.magdy.ahmed@gmail.com>
2023-08-11 17:16:24 -0700

    "commit"

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+/**
+ * main - entry point
+ *
+ * Descreption: print alphapet in lower case exept e and q
+ *
+ * Return: alwayes 0 (success)
+*/
+#include <stdio.h>
+int main(void)
+{
+	int i;
+
+	for (i = 97; i < 123; i++)
+	{
+		if (!(i == (int)'q' || i == (int)'e'))
+				putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}

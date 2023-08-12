642f8c5e53743e6cc69792a95d94bc46e72e9754
SamarMorsy <samar.magdy.ahmed@gmail.com>
2023-08-11 18:31:24 -0700

    "commit"

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+/**
+ * main - entry poitn
+ *
+ * Descreption: print all number of base10
+ *
+ * Return: alwayes 0 (success)
+*/
+#include <stdio.h>
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		printf("%d", i);
+	printf("\n");
+	return (0);
+}

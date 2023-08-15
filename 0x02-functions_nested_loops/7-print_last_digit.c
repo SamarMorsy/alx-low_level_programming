b7eaf20bbbd82f698272e652181f0bbc67bc51a5
SamarMorsy <samar.magdy.ahmed@gmail.com>
2023-08-15 15:08:07 -0700

    "commit"

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,8 +7,12 @@
 */
 int print_last_digit(int n)
 {
-	int mod_n = (n % 10);
+	int mod_n;
 
+	if(n > 0)
+		mod_n = (n % 10);
+	else
+		mod_n = (_abs(n) % 10);
 	_putchar(mod_n + '0');
 	return (mod_n);
 }

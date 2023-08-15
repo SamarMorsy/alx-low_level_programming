75bb379c1794d21843acab8e31a193cb3ff85cc4
SamarMorsy <samar.magdy.ahmed@gmail.com>
2023-08-15 12:10:25 -0700

    "commit"

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,13 +10,12 @@ void print_alphabet_x10(void)
 {
 	int i = 0;
 	int j ;
-	do
-       	{
-		for(j = 'a'; j <= 'z'; j++)
+
+	do{
+		for (j = 'a'; j <= 'z'; j++)
 			_putchar(j);
 		_putchar('\n');
 		i++;
-
 	}
 	while(i < 10);
 }

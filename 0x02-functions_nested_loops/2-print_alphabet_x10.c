58d80cb56c13b5a41d730ae99b3263859c915cc6
SamarMorsy <samar.magdy.ahmed@gmail.com>
2023-08-15 11:58:16 -0700

    "commit"

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ *  print_alphabet_x10 - print from a-z ten times
+ *
+ *  Descreption: print from a-z ten zimes
+ *
+ *  Return: Void
+*/
+void print_alphabet_x10(void)
+{
+	int i = 0;
+	int j ;
+	do
+       	{
+		for(j = 'a'; j < 'z'; j++)
+			_putchar(j);
+		_putchar('\n');
+
+	}
+	while(i < 10);
+}

1d5ab4d23e7f53ede5786955e51781353830a8de
SamarMorsy <samar.magdy.ahmed@gmail.com>
2023-08-16 05:13:38 -0700

    "commit"

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -11,16 +11,15 @@
 int _sum(int n)
 {
 	int i;
-	int sum;
-
+	int sum = 0;
 	for (i = 0; i < n; i++)
 	{
 		if (((i % 3) == 0) || ((i % 5) == 0))
 		{
 			sum += i;
 		}
-		return (sum);
 	}
+	return (sum);
 }
 /**
  * main - entry point

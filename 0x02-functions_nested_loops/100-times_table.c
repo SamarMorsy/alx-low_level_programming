d06f11182e52369c56f00a219bfd678f84fc0f41
SamarMorsy <samar.magdy.ahmed@gmail.com>
2023-08-15 21:49:52 -0700

    "commit"

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -15,9 +15,9 @@ void print_times_table(int n)
 		for (j = 1; j <= n; j++)
 		{
 			if ((i * j) > 9)
-				printf(", %d", (i * j));
-			else
 				printf(",  %d", (i * j));
+			else
+				printf(",   %d", (i * j));
 		}
 		printf("\n");
 	}

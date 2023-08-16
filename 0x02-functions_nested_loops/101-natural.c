d820ceeaf1398351053f15af107c28402fe7b0bf
SamarMorsy <samar.magdy.ahmed@gmail.com>
2023-08-16 05:04:49 -0700

    "commit"

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+/**
+ * _sum - add
+ *
+ *@n: input
+ *
+ * Descreption: add
+ *
+ * Return: sum
+*/
+int _sum(int n)
+{
+	int i;
+	int sum;
+
+	for (i = 0; i < n; i++)
+	{
+		if (((i % 3) == 0) || ((i % 5) == 0))
+		{
+			sum += i;
+		}
+		return (sum);
+	}
+}
+/**
+ * main - entry point
+ *
+ * Descreption: ay 7aga
+ *
+ * Return: 0
+*/
+int main(void)
+{
+	int sum = 0;
+
+	sum = _sum(1024);
+
+}

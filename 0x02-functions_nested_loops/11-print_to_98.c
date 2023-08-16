c2167a3ad1db6db54005ddd5516418bc5502be37
SamarMorsy <samar.magdy.ahmed@gmail.com>
2023-08-15 18:21:23 -0700

    "commit"

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,24 +8,14 @@ void print_to_98(int n)
 {
 	int i;
 
-	for (i = n; i <= 98; i++)
+	if (n >= 98)
 	{
-		if (i < 10)
-		{
-			putchar(i + '0');
-			putchar(',');
-			putchar(' ');
-		}
-		else
-		{
-			putchar((i / 10) + '0');
-			putchar((i % 10) + '0');
-			if (!(i == 98))
-			{
-			putchar(',');
-			putchar(' ');
-			}
-		}
+		for (i = n; i >= 98; i--)
+			printf("%d, ", i);
 	}
-	putchar('\n');
+	else if (n < 98)
+	{
+		for (i = n; i <= 98; i++)
+			printf("%d, ", i);
+	}
 }

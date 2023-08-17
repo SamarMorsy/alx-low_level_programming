cc9c0b2fb70f924016589849dfafaada89f11da5
SamarMorsy <samar.magdy.ahmed@gmail.com>
2023-08-16 17:55:11 -0700

    "commit"

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -6,23 +6,22 @@
 #include <stdio.h>
 int main(void)
 {
-	long int sum=2;
+	long int sum = 2;
 	long int first = 1;
 	long int second = 2;
 	long int counter = 3;
 	long int current = first + second;
-	do
-	{
-		if((current % 2) == 0)
+
+	do {
+		if ((current % 2) == 0)
 		{
 			sum += current;
 		}
 		first = second;
 		second = current;
 		current = first + second;
-		counter ++;
-		
-	} while(counter < 4000000);
-	printf("%lu\n"; sum);
+		counte++;
+	} while (counter < 4000000);
+	printf("%lu\n", sum);
 	return (0);
 }

72ba3db9635cbe8cf33f166da34e54ed88d00d94
SamarMorsy <samar.magdy.ahmed@gmail.com>
2023-08-17 15:58:09 -0700

    "commit"

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -5,6 +5,7 @@
  * @a: first integer
  * @b: second integer
  * @c: third integerReturn: largest number
+ * Return: largest
 */
 
 int largest_number(int a, int b, int c)
@@ -12,17 +13,11 @@ int largest_number(int a, int b, int c)
 	int largest;
 
 	if (a >= b && a >= c)
-	{
 		largest = a;
-	}
 	else if (b >= a && b >= c)
-	{
 		largest = b;
-	}
 	else
-	{
 		largest = c;
-	}
 
 	return (largest);
 }

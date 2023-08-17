4a6f3cd2522990d5156abe376d6ef709e5b821ff
SamarMorsy <samar.magdy.ahmed@gmail.com>
2023-08-17 16:52:26 -0700

    "commit"

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isupper - check upper case
+ * @c: input
+ * Return: 1 ia upper 0 if lower
+*/
+
+int _isupper(int c)
+{
+	if (x >= 65 && x <= 90)
+		return (1);
+	else
+		return (0);
+}
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,6 @@
+#ifndef MAIN_H
+#define MAIM_H
+
+int _isupper(int c);
+
+#endif

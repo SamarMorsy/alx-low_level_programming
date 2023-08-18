c5757f3a10dcd9a558a326c78b0f4bf7ce54e758
SamarMorsy <samar.magdy.ahmed@gmail.com>
2023-08-17 19:32:36 -0700

    "commit"

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_diagonal - print diagonal
+ *
+ * @n: inpit
+ *
+ * Return: void
+*/
+
+void print_diagonal(int n)
+{
+	int i;
+	int j;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < (n - 1); j++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\');
+			_putchar('\n');
+		}
+	}
+}

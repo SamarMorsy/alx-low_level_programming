0902932d7c3d181df28ce9e871dd56b225284ff2
SamarMorsy <samar.magdy.ahmed@gmail.com>
2023-08-18 07:12:16 -0700

    "commit"

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -10,19 +10,25 @@
 
 void print_triangle(int size)
 {
-	int i, j, k;
-
-	for (i = 0; i < size; i++)
+	if (size <= 0)
 	{
-		for (j = ((size - 1) - i); j > 0; j--)
-		{
-			_putchar(' ');
-		}
-		for (k = 0; k <= i; k++)
-		{
-			_putchar('#');
-		}
-
 		_putchar('\n');
+	}
+	else
+	{
+		int i, j, k;
+
+		for (i = 0; i < size; i++)
+		{
+			for (j = ((size - 1) - i); j > 0; j--)
+			{
+				_putchar(' ');
+			}
+			for (k = 0; k <= i; k++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
 	}
 }

2e43eb15c98b5651ac5d36b97cafc9645dca4ec2
SamarMorsy <samar.magdy.ahmed@gmail.com>
2023-08-17 18:38:34 -0700

    "commit"

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -12,10 +12,12 @@ void more_numbers(void)
 	int j;
 
 	for (i = 0; i < 10; i++)
+	{
 		for (j = 0; j < 15; j++)
 		{
 			_putchar(j + '0');
 			_putchar('\n');
 		}
-	_putchar('\n);
+	}
+	_putchar('\n');
 }

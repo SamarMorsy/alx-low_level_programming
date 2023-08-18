69b6eb339031df9eba018d69c5e166a08aff0eaa
SamarMorsy <samar.magdy.ahmed@gmail.com>
2023-08-18 05:49:24 -0700

    "commit"

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -17,14 +17,18 @@ int main(void)
 		else if ((i % 3) == 0)
 			printf("Fizz ");
 		else if ((i % 5) == 0)
-			printf("Buzz ");
-		else if (i == 100)
-			printf("%d\n", i);
+		{
+			if (i == 100)
+				printf("Buzz");
+			else
+				printf("Buzz ");
+		}
 		else
 			printf("%d ", i);
 
 	}
 
+	printf("\n");
 	return (0);
 }
 

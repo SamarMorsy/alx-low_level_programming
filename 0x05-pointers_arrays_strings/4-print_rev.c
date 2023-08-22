95ec0a36cfccf0127707a108e0d69581b55271ec
SamarMorsy <samar.magdy.ahmed@gmail.com>
2023-08-22 15:50:33 -0700

    "commit"

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,11 +8,12 @@
 */
 void print_rev(char *s)
 {
-	int i;
-	int l = _strlen(s) - 1;
+	int i = 0;
 
-	s = s + l;
-	for (i = l; i >= 0; i--)
+	while (*s[i] != '\0')
+		i++;
+
+	while (i != 0)
 	{
 		_putchar(*s);
 	}

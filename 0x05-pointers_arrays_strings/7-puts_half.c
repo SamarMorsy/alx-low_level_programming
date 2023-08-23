4ad5777a78ff023ede7a3718278b9dc9b9b894dd
SamarMorsy <samar.magdy.ahmed@gmail.com>
2023-08-23 09:55:38 -0700

    "commit"

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -12,12 +12,8 @@ void puts_half(char *str)
 
 	while (str[length] != '\0')
 		length++;
-	if (length % 2 == 0)
-		start_index = length / 2;
-	else
-		start_index = (length - 1) / 2;
-
-	for (i = start_index; i < length; i++)
+	start_index = length / 2;
+	for (i = istart_index; i < length ; i++)
 	{
 		_putchar(str[i]);
 	}

4b47987ab89fa9340c268219ad68d80e8d0ab2de
SamarMorsy <samar.magdy.ahmed@gmail.com>
2023-08-22 18:06:07 -0700

    "commit"

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main."
+/**
+ * rev_string - reverse string
+ * @s: input
+*/
+void rev_string(char *s)
+{
+	int l = 0;
+	inti i;
+	char *begin, *end, temp;
+
+	while (s[l] != '\n')
+		l++;
+	begin = s;
+	end = s + l - 1;
+	for (i = 0; i < (l - 1) / 2; i++)
+	{
+		temp = *end;
+		*end = *begin;
+		*begin = temp;
+		begin++;
+		end--;
+	}
+}

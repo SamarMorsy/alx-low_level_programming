39cef2f2db15acc6f5a49406de1d1f18e135efc4
SamarMorsy <samar.magdy.ahmed@gmail.com>
2023-08-23 09:21:38 -0700

    "commit"

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * puts_half - print second halve of string
+ * @str:input
+*/
+
+void puts_half(char *str)
+{
+	int length = 0;
+	int start_index;
+	int i;
+
+	while (str[length] != '\0')
+		length++;
+	if (length % 2 == 0)
+		strart_index = length / 2;
+	else
+		start_index = (length - 1) / 2;
+
+	for (i = start_index; i < length; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}

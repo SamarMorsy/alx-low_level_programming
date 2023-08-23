b6a20243ea3a5dcfb5400aff128f514ec428cbd0
SamarMorsy <samar.magdy.ahmed@gmail.com>
2023-08-23 10:23:38 -0700

    "commit"

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_array - print
+ * @a: inout
+ * @n: length
+*/
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i <= n; i++)
+	{
+		if (i == n)
+			printf("%d\n", a[i]);
+		printf("%d, ", a[i]);
+	}
+}

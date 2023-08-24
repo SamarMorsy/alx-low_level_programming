7a56b5ef0837ebf5dcf63f09a1437dcbeede59bc
SamarMorsy <samar.magdy.ahmed@gmail.com>
2023-08-24 13:33:45 -0700

    "commit"

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * _strncat - concatinate specific number of string
+ * @dest: input
+ * @src: input
+ * @n: input
+ * Return: dest
+*/
+char *_strncat(char *dest, char *src, int n)
+{
+	if (n > 0)
+	{
+		int destLen = 0;
+		int scrLen = 0;
+		int i;
+
+		while (dest[destLen] != '\0')
+			destLen++;
+		while (src[scrLen] != '\0')
+			scrLen++;
+		for (i = 0; i <= n; i++)
+		{
+			if (i == n)
+				dest[destLen + i] = '\0';
+			else
+				dest[destLen + i] = src[i];
+		}
+	}
+	return (dest);
+}
+
+

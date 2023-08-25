0c9bb12df9218334ab31b139efb8d35ef7a01d7a
SamarMorsy <samar.magdy.ahmed@gmail.com>
2023-08-25 08:30:50 -0700

    "commit"

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * _strncpy - copy string
+ * @dest: input
+ * @src: input
+ * @n: input
+ * Return: dest
+*/
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+	int srcLen = 0;
+	int destLen = 0;
+
+	if (n > 0)
+	{
+		while (dest[destLen] != '\0')
+			destLen++;
+		while (src[srcLen] != '\0')
+			srcLen++;
+		if (n > srcLen)
+			n = srcLen;
+		for (i = 0; i < n; i++)
+		{
+			dest[i] = src[i];
+		}
+		dest[srcLen] = '\0';
+	}
+	return dest;
+}

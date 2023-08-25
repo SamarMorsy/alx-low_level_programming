0cf0737f242ab3f2a1f29a962086d394fb1c12e4
SamarMorsy <samar.magdy.ahmed@gmail.com>
2023-08-25 13:16:18 -0700

    "commit"

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -9,22 +9,15 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
-	int srcLen = 0;
-	int destLen = 0;
 
-	if (n > 0)
+	for (i = 0; (i < n) && (src[i] != '\0'); i++)
 	{
-		while (dest[destLen] != '\0')
-			destLen++;
-		while (src[srcLen] != '\0')
-			srcLen++;
-		if (n > srcLen)
-			n = srcLen;
-		for (i = 0; i < n; i++)
-		{
-			dest[i] = src[i];
-		}
-		dest[srcLen] = '\0';
+		dest[i] = src[i];
 	}
-		return (dest);
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return (dest);
 }

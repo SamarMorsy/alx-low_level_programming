d0771f0a2b88b29df77a4d345e85cfa1f946ee5b
SamarMorsy <samar.magdy.ahmed@gmail.com>
2023-08-25 14:27:18 -0700

    "commit"

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strcmp - compare two strings
+ * @s1: input
+ * @s2: input
+ * Return: int
+*/
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0;
+
+	while (s1[i] != '\0' && s2[i] != '\0')
+	{
+		if (s1[i] == s2[i])
+			i++;
+		else if (s1[i] > s2[i])
+			return (1);
+		else if (s1[i] < s2[i])
+			return (-1);
+	}
+	return (0);
+}

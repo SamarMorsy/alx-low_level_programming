6cedd8086061ce52e116a009371091af2972eb96
SamarMorsy <samar.magdy.ahmed@gmail.com>
2023-08-29 06:43:42 -0700

    "commit"

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -10,8 +10,6 @@ char *_strstr(char *haystack, char *needle)
 	int i;
 	int j;
 
-	if (*needle == '\0')
-		return (NULL);
 	for (i = 0; haystack[i] != '\0'; i++)
 	{
 		for (j = 0; needle[j] != '\0'; j++)

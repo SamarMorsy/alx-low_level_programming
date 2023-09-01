a3cf308ca726f7b97a768379b5e1f61c3a67219e
SamarMorsy <samar.magdy.ahmed@gmail.com>
2023-08-31 19:24:09 -0700

    "commit"

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _print_rev_recursion - print str in reverse
+ * @s: input
+*/
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_putchar(s + 1);
+		_putchar(*s);
+	}
+}

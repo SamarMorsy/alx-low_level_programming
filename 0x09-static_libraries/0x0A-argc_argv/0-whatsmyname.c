27dd6fe383401313108792a519a4f612c3d948dd
SamarMorsy <samar.magdy.ahmed@gmail.com>
2023-09-02 10:43:54 -0700

    "commit"

---
+++ 0x09-static_libraries/0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - entry
+ * @argc: int
+ * @argv: array
+ * Return: 0
+*/
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s", argv[0]);
+	return (0);
+}
+
---
+++ 0x09-static_libraries/0x0A-argc_argv/1-args.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - entry
+ * @argc: int
+ * @argv: array
+ * Return: (0)
+*/
+int main(int argc, char* argv)
+{
+	printf(
+}

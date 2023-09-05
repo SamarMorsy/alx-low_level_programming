9c6b66e2930e4be4da9937a47c04304be70f1b96
SamarMorsy <samar.magdy.ahmed@gmail.com>
2023-09-05 11:25:51 -0700

    "commit"

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * create_array - create_array
+ * @size: array length
+ * @c: char
+ * Return: pointer to char
+*/
+char *create_array(unsigned int size, char c)
+{
+	int i;
+	char *p;
+
+	if (size == 0 || c == '\0')
+		return (NULL);
+	p = malloc(sizeof(char) * size);
+	for (i = 0; i < size; i++)
+	{
+		p[i] = c;
+	}
+	free(p);
+	return (p);
+}

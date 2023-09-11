f78577f3564abecbdd6c0fc6467ff2da9910eb84
SamarMorsy <samar.magdy.ahmed@gmail.com>
2023-09-10 17:00:12 -0700

    "commit"

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -8,15 +8,15 @@
 */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	int i = 0;
-	int l1 = 0;
-	int l2 = 0;
+	unsigned int i = 0;
+	unsigned int l1 = 0;
+	unsigned int l2 = 0;
 	char *p;
 
-	if (*s1 == NULL)
-		*s1 = "";
-	if (*s2 == NULL)
-		*s2 = "";
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 	while (s1[i] != '\0')
 	{
 		l1++;
@@ -30,8 +30,6 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	}
 	if (n >= l2)
 		n = l2;
-	if (n < 0)
-		return (NULL);
 	p = malloc(l1 + n + 1);
 	if (p == NULL)
 		return (NULL);

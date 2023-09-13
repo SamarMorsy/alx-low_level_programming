51508181cb0adb65804cd84c35263fc2e99682e0
SamarMorsy <samar.magdy.ahmed@gmail.com>
2023-09-13 16:12:40 -0700

    "commit"

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+#include <stdio.h>
+/**
+ * print_dog - fun
+ * @d: dog
+*/
+void print_dog(struct dog *d)
+{
+	if (d)
+	{
+		printf("Name: %s\n", d->name ? d->name : "(nil)");
+		printf("Age: %d\n", d->age);
+		printf("Owner: %s\n", d->owner ? d->owner : "(nil)");
+	}
+}

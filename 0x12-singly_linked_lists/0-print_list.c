b4a9de027de569d9fe3b8633ad08d1b8434b3d81
root <root@DESKTOP-JDKALDU>
2023-09-21 14:27:25 +0300

    1

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,41 @@
+#include "lists.h"
+/**
+ * str_len - functoin
+ * @s: pointer to strung
+ * Return:len
+*/
+int str_len(char *s)
+{
+	int i;
+	int len = 0;
+
+	for (i = 0, s[i] != '\0'; i++)
+	{
+		len++;
+	}
+	return (len);
+}
+
+/**
+ * print_list - func to print
+ * @h: node
+ * Return: int
+*/
+size_t print_list(const list_t *h)
+{
+	list_t *current = h;
+	size_t s = 0;
+	int str_len = str_len(current->str);
+
+	while (current != NULL)
+	{
+		if (current->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s", str_len, current->str);
+		currenrt = current->next;
+		s++;
+	}
+	return (s);
+}
+

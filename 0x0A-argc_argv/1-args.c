4f25267dfc332f9cf24f4ef961bc8d5e4709cbf6
root <root@DESKTOP-JDKALDU>
2023-10-03 00:04:11 +0300

    FF

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * @argc: num og args
+ * @argv: list of args
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	(void) *argv[];
+	printf("%d\n", argc);
+	return (0);
+}

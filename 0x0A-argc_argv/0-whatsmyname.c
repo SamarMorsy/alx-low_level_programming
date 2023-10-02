54b69a0786d3d5771e42888803529273a8ea2a59
root <root@DESKTOP-JDKALDU>
2023-10-02 23:47:07 +0300

    d

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.g>
+/**
+ * main - entry point
+ * @argc: number of args
+ * @argv: array of args
+ * Return: 0
+*/
+int main(int argc, char const *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}

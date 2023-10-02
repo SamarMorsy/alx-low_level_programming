02abfe696cf0326c125ace5ff2dae7c6582ba0dc
root <root@DESKTOP-JDKALDU>
2023-10-03 00:29:00 +0300

    D

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -9,8 +9,9 @@ int main(int argc, char const *argv[])
 {
 	int i;
 
-	for (i = 0, i < argc; i++)
+	for (i = 0; i < argc; i++)
 	{
-		printf("%d\n", argv[i]);
+		printf("%s\n", argv[i]);
 	}
+	return (0);
 }

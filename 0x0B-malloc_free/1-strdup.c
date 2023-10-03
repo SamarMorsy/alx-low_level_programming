91fa3c080796b318b2d1e1f0f200a443580942a0
root <root@DESKTOP-JDKALDU>
2023-10-03 18:32:57 +0300

    Ff

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strdup - func
+ * @str: str to cop
+ * Return: pointer to new str
+*/
+char *_strdup(char *str)
+{
+	char *new_str;
+	int i;
+	int len;
+
+	if (str == NULL)
+		return (NULL);
+	for (i = 0; str[i] != '\0'; i++)
+		len++;
+	new_str = malloc(sizeof(char) * len);
+	if (!new_str)
+		return (NULL);
+
+	for  i = 0; i <= len; i++)
+		new_str[i] = str[i];
+	return (new_str);
+}

77b7d14cfe4507f263d55317b7ef7d97595a787e
root <root@DESKTOP-JDKALDU>
2023-10-03 17:46:01 +0300

    d

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -7,11 +7,12 @@
 */
 char *create_array(unsigned int size, char c)
 {
+	char *arr;
+        unsigned int i;
+
+
 	if (size == 0)
 		return (NULL);
-	char *arr;
-	unsigned int i;
-
 	arr = malloc(size * sizeof(char));
 	if (!arr)
 		return (NULL);

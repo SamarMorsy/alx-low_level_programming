c7e2a6216ce50e9c29c740ee001b8d7e791252c8
root <root@DESKTOP-JDKALDU>
2023-10-04 00:54:24 +0300

    w

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -10,11 +10,7 @@ void free_grid(int **grid, int height)
 
 	for (i = 0; i < height; i++)
 	{
-		if (!grid[i])
-		{
-			while (i--)
-				free(grid[i]);
-			free(grid);
-		}
+		free(grid[i]);
+		free(grid);
 	}
 }

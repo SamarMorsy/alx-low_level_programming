38f0634fedfab3f5bfdf85e7ee74a47801092baf
root <root@DESKTOP-JDKALDU>
2023-10-04 00:17:55 +0300

    ss

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * alloc_grid - fun
+ * @width: w
+ * @height: h
+ * Return: arr
+*/
+int **alloc_grid(int width, int height)
+{
+	int i = 0;
+	int j = 0;
+	int **grid;
+
+	if ((width <= 0) || (height <= 0))
+		return (NULL);
+	grid = malloc(sizeof(*grid) * height); /* height is the number of rows*/
+	if (!grid)
+		return (NULL);
+	for (i = 0; i < height; i++)
+	{
+		grid[i] = malloc(sizeof(**grid) * width);
+	}
+	if (!grid)
+		return (NULL);
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+			grid[i][j] = 0;
+	}
+	return (grid);
+}

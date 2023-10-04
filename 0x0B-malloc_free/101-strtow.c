57d5acc88da80129f96b8eeac3c1c2dd51a80136
root <root@DESKTOP-JDKALDU>
2023-10-04 23:52:16 +0300

    e

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include "string.h"
+/**
+ * strtow - fi
+ * @str: jj#
+ * Return: j
+*/
+char **strtow(char *str)
+{
+	int i = 0;
+	char **result;
+	char *tocken;
+	char *remainder = str;
+	int nWords = 0;
+
+	if (str == NULL)
+		return (NULL);
+	tocken = strtok(str, " ");
+	while (tocken != NULL)
+	{
+		nWords++;
+		token = strtok(NULL, " ");
+	}
+	result = malloc(nWords * sizeof(char *) + 1);
+	if (result == NULL)
+		return (NULL);
+	while ((token = strtok(remainder, " ")) != NULL)
+	{
+		result[i] = strdup(token);
+		i++;
+		remainder = NULL;
+	}
+	result[nWords] = NULL;
+	return (result);
+}

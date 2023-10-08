f6cc29edb1f12789a0e136d89990900c7b4f3b71
root <root@DESKTOP-JDKALDU>
2023-10-09 00:43:47 +0300

    jf

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -3,9 +3,8 @@
 /**
   *array_range - creates an array of integers.
   *@min: minimum value.
-  *@max: maximum value.
-  *
-  *Return:
+  *@max: maximum
+  *Return:khk
   */
 int *array_range(int min, int max)
 {

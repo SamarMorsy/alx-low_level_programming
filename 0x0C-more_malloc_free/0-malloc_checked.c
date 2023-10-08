c8a9c8e68d37157f688403456e0feff0dfc0196a
root <root@DESKTOP-JDKALDU>
2023-10-09 00:31:05 +0300

    jf

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+*malloc_checked - allocates memory using malloc
+*@b: bytes
+*Return: pointer to nothing
+*/
+void *malloc_checked(unsigned int b)
+{
+	int *p;
+
+	p = malloc(b);
+	if (p == NULL)
+		exit(98);
+	return (p);
+}

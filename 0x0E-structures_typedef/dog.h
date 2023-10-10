bdac9d0197e891199eb8325c5657efa75fd27227
root <root@DESKTOP-JDKALDU>
2023-10-10 19:41:59 +0300

    f

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * struct dog - struct
+ * @name: name
+ * @age: age
+ * @owner: owner
+*/
+
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+}
+
+#endif

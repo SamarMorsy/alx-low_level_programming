c5d316a321d28954369e086376894bc71a6a8169
root <root@DESKTOP-JDKALDU>
2023-10-10 19:52:05 +0300

    f

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,14 @@
+#include "dog.h"
+/**
+ * init_dog - function
+ * @d: dz
+ * @name: xx
+ * @age:sss
+ * @owner: dd
+*/
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}

911137ff8810856588da0d08a4e09eb2238c9caa
root <root@DESKTOP-JDKALDU>
2023-10-13 19:00:58 +0300

    K

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,5 +1,6 @@
 #ifndef FUNCTION_POINTERS
 #define FUNCTION_POINTERS
-#include <stdio.h>
+#include <stdio.hi>
 void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
 #endif

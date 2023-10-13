9653070f0dbf668e2792723b05bc232f64dfacf9
root <root@DESKTOP-JDKALDU>
2023-10-13 17:31:55 +0300

    K

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+#include "function_pointers.h"
+/**
+ * print_name - function
+ * @name: string (pointer to char)
+ * @f: pointer to function tha taks pointer to char
+*/
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
